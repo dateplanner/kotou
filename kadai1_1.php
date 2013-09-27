34af8e9786e3f3a4626f34d3fddfb5a70ea53265
dateplanner <1201259@st.asojuku.ac.jp>
2013-09-27 11:42:29 +0900

    コミット

---
+++ kadai1_1.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title></title>
+<h2>新消費税計算</h2>
+<body>
+<form method="post" action="kadai1_2.php">
+金額：<input type="text" name="kane" />
+<input type="submit" name="button1" value="送信" />
+</form>
+</body>
+</html>
