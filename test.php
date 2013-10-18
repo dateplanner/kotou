42195e54cceb663221c6f5a9a3b367f0ad903245
dateplanner <1201259@st.asojuku.ac.jp>
2013-10-18 12:45:48 +0900

    komitto

--- test.php
+++ test.php
@@ -3,5 +3,10 @@ bbbbbbbbbbbbbbb
 aaaaaaaaaaaaaaa
 bbbbbbbbbbbbbbbb
 ccccccccccccccc
+<<<<<<< HEAD
 eeeeeeeeeeeeeee
 ddddddddddddddd
+=======
+ddddddddddddddd
+
+>>>>>>> 6b27b095442e41c0ef9055918df7ae50cb092641
